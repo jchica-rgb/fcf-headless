fe6dd829af2a1383e96529b8751d5c04d9648a64
jchica-rgb <jchica@territorifutcat.es>
2026-07-18 13:59:12 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "db5020951893.hosting-data.io",
-  user: "dbu3815614",
+  host: "localhost",
+  user: "futcat_user",
   password: "TerritoriFutcat/2026",
-  database: "dbs15909928",
-  port: 3306,
+  database: "futcat",
   waitForConnections: true,
   connectionLimit: 10
 });
